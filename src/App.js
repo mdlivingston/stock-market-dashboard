bb6c751d57800ee929ec0e813799081eacfc4711
Maximus <developermaximus@gmail.com>
2021-01-23 17:57:10 -0600

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import SP500 from './Components/SP500';
 
-function App() {
+export default function App()
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3 class="header"> US ECONOMIC HEALTH DASHBOARD</h3>
+      <div class="row">
+        <div class="inner"></div>
+        <div class="inner"></div>
+      </div>
+      <div class="content">
+        <SP500 className={'card'}></SP500>
+      </div>
+      <div class="row">
+        <div class="inner"></div>
+        <div class="inner"></div>
+      </div>
     </div>
   );
 }
 
-export default App;
---
+++ src/Components/SP500.js
@@ -0,0 +1,170 @@
+import * as React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import Chart from "react-apexcharts";
+import { Dropdown, Button } from 'react-bootstrap';
+
+function getPreviousDate(subtractedDays)
+{
+    const date = new Date();
+    date.setDate(date.getDate() - subtractedDays)
+    return date;
+}
+
+async function fetchStock(dayRange)
+{
+
+    const result = await axios(
+        `https://api.twelvedata.com/time_series?symbol=GSPC&interval=1day&apikey=${process.env.REACT_APP_STOCK_API_KEY}&source=doc&start_date=${getPreviousDate(dayRange).toISOString()}`,
+    );
+    console.log(result.data.values)
+    return result.data.values ? result.data.values : result.data;
+}
+
+async function fetchSMA(interval, dayRange)
+{
+
+    const result = await axios(
+        `https://api.twelvedata.com/ma?symbol=GSPC&interval=1day&apikey=${process.env.REACT_APP_STOCK_API_KEY}&source=doc&start_date=${getPreviousDate(dayRange).toISOString()}&time_period=${interval}`,
+    );
+    console.log(result.data.values)
+    return result.data.values;
+}
+
+export default function SP500()
+{
+    const [data, setData] = useState([]);
+    const [dayRangeString, setDayRangeString] = useState('Half a year');
+    const [error, setError] = useState('');
+
+    useEffect(() =>
+    {
+        fetchData('6 Months', 180);
+    }, []);
+
+    return (
+        data.length === 0 ?
+            <div style={{ 'textAlign': 'center', 'color': 'white' }}>
+                <p>{error}</p>
+                <Button variant="primary" onClick={() => fetchData('6 Months', 180)}>Refresh</Button>
+            </div> :
+            <div style={{ paddingLeft: 20, paddingRight: 20, backgroundColor: 'black' }}>
+                <Dropdown>
+                    <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                        {dayRangeString}
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={() => fetchData('6 Months', 180)}>6 Months</Dropdown.Item>
+                        <Dropdown.Item onClick={() => fetchData('1 Year', 365)}>1 Year</Dropdown.Item>
+                        <Dropdown.Item onClick={() => fetchData('2 Years', 700)}>2 years</Dropdown.Item>
+                        <Dropdown.Item onClick={() => fetchData('5 Years', 1825)}>5 Years</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Chart
+                    options={data.options}
+                    series={data.series}
+                    type="line"
+                    height="500"
+                /></div>
+
+    );
+
+    async function fetchData(rangeString, range)
+    {
+
+        const stockData = await fetchStock(range);
+
+        if (stockData.message)
+        {
+            setError(stockData.message)
+        }
+        else
+        {
+            const sma50 = await fetchSMA(50, range);
+            const sma100 = await fetchSMA(100, range);
+            setDayRangeString(rangeString)
+            const chartData = {
+
+                options: {
+                    title: {
+                        text: 'S&P 500',
+                        style: {
+                            color: 'white'
+                        },
+                        align: 'center'
+                    },
+                    chart: {
+                        id: "basic-line"
+                    },
+                    xaxis: {
+                        categories: stockData.map(dates => dates.datetime).reverse(),
+                        labels: {
+                            hideOverlappingLabels: true,
+                            offsetX: 20,
+                            style: {
+                                colors: 'white'
+                            }
+                        },
+                        axisTicks: {
+                            show: true,
+                            borderType: 'solid',
+                            color: 'grey',
+                            height: 6,
+                            offsetX: 5,
+                            offsetY: 0
+                        },
+                        tickAmount: 20
+                    },
+                    stroke: {
+                        show: true,
+                        curve: 'smooth',
+                        lineCap: 'butt',
+                        colors: undefined,
+                        width: 2,
+                        dashArray: 0,
+                    },
+                    yaxis: [{
+
+                        labels: {
+                            formatter: function (val)
+                            {
+                                return val.toFixed(0)
+                            },
+                            style: {
+                                colors: 'white'
+                            }
+                        },
+                        opposite: true
+                    }],
+                    legend: {
+                        show: true,
+                        position: 'bottom',
+                        horizontalAlign: 'center',
+                        floating: true,
+                        labels: {
+                            colors: 'white'
+                        },
+
+                    }
+                },
+                series: [
+                    {
+                        name: "Close",
+                        data: stockData.map(v => Number(v.close)).reverse(),
+
+                    },
+                    {
+                        name: "50 SMA",
+                        data: sma50.map(v => Number(v.ma)).reverse()
+                    },
+                    {
+                        name: "100 SMA",
+                        data: sma100.map(v => Number(v.ma)).reverse()
+                    }
+                ],
+            };
+            setData(chartData);
+        }
+    }
+}
